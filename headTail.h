53c4f19849b19bbc0bdf07cd45fa1b7a698c86d1
du-xiang​​ <2876069311@qq.com>
2023-04-13 22:19:33 +0800

    日常提交

---
+++ headTail.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"getTokens.h"
+
+const std::string head = "\
+<!DOCTYPE html>\n\
+<html lang=zh-cmn-Hans>\n\
+<head>\n\
+	<meta charset = \"utf-8\">\n\
+	<title>markdownParser</title >\n\
+</head>\n\
+<body>\n";
+
+const std::string tail = "\
+</body>\n\
+</html>";
