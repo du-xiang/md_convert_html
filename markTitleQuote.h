7f86f68f9269d27dc3601875790b5b4fb701f233
du-xiang​​ <2876069311@qq.com>
2023-04-13 22:21:30 +0800

    日常提交

--- markTitleQuote.h
+++ markTitleQuote.h
@@ -1,95 +1,107 @@
-#include "markdownParser.h"
+#pragma once
+
+#include "markListTable.h"
 
 
 // 行内标记
-Token markdownParser::markInline(const std::string &line){
+std::vector<Token> markdownParser::markInline(const std::string& line) {
+	std::vector<Token> tokens;
 	Token tokenText(TokenType::TEXT);
 	tokenText.content = line;
 
-	return tokenText;
+	tokens.push_back(tokenText);
+
+	return tokens;
 }
 
 //	标题标记
-Token markdownParser::markTitle(const std::string &line){
+Token markdownParser::markTitle(const std::string& line) {
 	Token tokenTitle(TokenType::TITLE);			//	标题 Token
-	Token tokenText(TokenType::TEXT);			//	文本 Token
+	Token tokenPara(TokenType::PARAGRAPH);		//	段落 Token
 	int pos = 0;								//	当前字符位置
-	int count =0;								//	统计 '#' 个数
+	int count = 0;								//	统计 '#' 个数
 	bool isTitle = true;						//	判断 Token 是否为 title
 
-	while(pos < line.length() && line[pos] == '#'){
-		pos   += 1;
+	while (pos < line.length() && line[pos] == '#') {
+		pos += 1;
 		count += 1;
 	}
 
-	if(line[pos] == ' '){
-		switch(count){
-			case 1:
-				tokenTitle.level = 1;
-				break;
-			case 2:
-				tokenTitle.level = 2;
-				break;
-			case 3:
-				tokenTitle.level = 3;
-				break;
-			case 4:
-				tokenTitle.level = 4;
-				break;
-			case 5:
-				tokenTitle.level = 5;
-				break;
-			case 6:
-				tokenTitle.level = 6;
-				break;
-			default:
-				isTitle = false;
+	if (line[pos] == ' ') {
+		switch (count) {
+		case 1:
+			tokenTitle.level = 1;
+			break;
+		case 2:
+			tokenTitle.level = 2;
+			break;
+		case 3:
+			tokenTitle.level = 3;
+			break;
+		case 4:
+			tokenTitle.level = 4;
+			break;
+		case 5:
+			tokenTitle.level = 5;
+			break;
+		case 6:
+			tokenTitle.level = 6;
+			break;
+		default:
+			isTitle = false;
 		}
 	}
+	else
+		isTitle = false;
 
-	if(isTitle && pos+1 < line.length()){
-		markInline(line.substr(pos+1));
-	}else if(isTitle && pos+1 >= line.length()){
+	if (isTitle) {
+		if(pos + 1 < line.length())				//	防止.substr() 函数溢出
+			tokenTitle.children = markInline(line.substr(pos + 1));
 		return tokenTitle;
-	}else{
-		return tokenText;
+	}
+	else {
+		if (pos + 1 < line.length())
+			tokenPara.children = markInline(line.substr(pos + 1));
+		return tokenPara;
 	}
 }
 
 // 引用标记
-Token markdownParser::markQuote(const std::string &line){
-    Token tokenQuote(TokenType::QUOTE);			//	引用 Token
-	Token tokenText(TokenType::TEXT);			//	文本 Token
+Token markdownParser::markQuote(const std::string& line) {
+	Token tokenQuote(TokenType::QUOTE);			//	引用 Token
+	Token tokenPara(TokenType::PARAGRAPH);		//	段落 Token
 	int pos = 0;								//	当前字符位置
-	int count =0;								//	统计 '>' 个数
+	int count = 0;								//	统计 '>' 个数
 	bool isQuote = true;						//	判断 Token 是否为 quote
 
-    while(pos < line.length() && line[pos] == '>'){
-		pos   += 1;
+
+	while (pos < line.length() && line[pos] == '>') {
+		pos += 1;
 		count += 1;
 	}
 
-    if(line[pos] == ' '){
-		switch(count){
-			case 1:
-				tokenQuote.level = 1;
-				break;
-			case 2:
-				tokenQuote.level = 2;
-				break;
-			case 3:
-				tokenQuote.level = 3;
-				break;
-			default:
-				isQuote = false;
-		}
+	switch (count) {
+	case 1:
+		tokenQuote.level = 1;
+		break;
+	case 2:
+		tokenQuote.level = 2;
+		break;
+	case 3:
+		tokenQuote.level = 3;
+		break;
+	default:
+		isQuote = false;
 	}
 
-    if(isQuote && pos+1 < line.length()){
-		markInline(line.substr(pos+1));
-	}else if(isQuote && pos+1 >= line.length()){
+	if (isQuote) {
+		if (pos < line.length())				//	防止.substr() 函数溢出
+			tokenQuote.children = markInline(line.substr(pos));
 		return tokenQuote;
-	}else{
-		return tokenText;
+	}
+	else {
+		if (pos < line.length())				//	防止.substr() 函数溢出
+			tokenPara.children = markInline(line.substr(pos));
+		return tokenPara;
 	}
 }
