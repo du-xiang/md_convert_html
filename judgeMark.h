0221447ac46281fafa2df365e095eb515325b7a7
du-xiang <2876069311@qq.com>
2023-04-15 22:31:58 +0800

    添加表格标记检测

--- judgeMark.h
+++ judgeMark.h
@@ -3,9 +3,9 @@
 
 
 bool markdownParser::isWrap(const std::string& line, const int& pos) {
-	char ch = line[pos];					//	此行出现的除空格外第一个字符
-	int countChar = 0;								//	统计出现的非空格字符数
-	int posWrap = pos;							//	字符定位
+	char ch = line[pos];								//	此行出现的除空格外第一个字符
+	int countChar = 0;									//	统计出现的非空格字符数
+	int posWrap = pos;									//	字符定位
 	if (ch == '*' || ch == '-' || ch == ' ') {
 		for (posWrap; posWrap < line.length(); posWrap++) {
 			if (line[posWrap] == ch)
@@ -44,5 +44,18 @@ bool markdownParser::isList(const std::string& line, const int& pos, bool& isOrd
 
 
 bool markdownParser::isTable(const std::string& line) {
-	return false;
+	std::string lineTemp = line;
+
+	//	先去除两端的空格字符
+	if (!lineTemp.empty())
+	{
+		lineTemp.erase(0, lineTemp.find_first_not_of(" "));
+		lineTemp.erase(lineTemp.find_last_not_of(" ") + 1);
+	}
+
+	if (lineTemp[0] == '|' && lineTemp[lineTemp.length() - 1] == '|' && lineTemp.length() != 1)
+		return true;
+	else
+		return false;
+
 }
