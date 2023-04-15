346eb233932c73a28ea2a769d6c6473aa9748505
du-xiang <2876069311@qq.com>
2023-04-15 15:27:05 +0800

    markList()函数封装

--- getTokens.h
+++ getTokens.h
@@ -15,7 +15,6 @@ std::vector<Token> markdownParser::getTokens(const std::string& mdName) {
 	Token markMath(TokenType::MATH);					//	存放数学公式内容Token
 	Token markCode(TokenType::CODE);					//	存放代码内容Token
 	Token markTable(TokenType::TABLE);					//	存放表格Token
-	Token tokenListCell(TokenType::LISTCELL);			//	创建列表 cell token 存放列表信息
 
 	std::ifstream mdFile(mdName);                       //	打开文件
 
@@ -117,46 +116,7 @@ std::vector<Token> markdownParser::getTokens(const std::string& mdName) {
 			markMath.content += (lineText+"\n");
 			break;
 		case parserStates::parserStateList:
-			Token* lastTokenPtr;						//	指向vector最后一个token
-			tokenListCell.children = markInline(lineText.substr(pos+2));
-
-			//	tokens为空、tokens 最后一个 TokenType 不是 LIST、当前空格数小于 LIST 空格数
-			//	以上三种情况需要新建一个 LIST 并将 tokenListCell 插入其中
-			if (tokens.size() != 0) {
-				Token* lastTokenPtr = &(tokens.back());
-				std::vector<Token>* tokensPtr = &(tokens);
-				//	token 类型不为列表，或LIST空格数大于等于当前列表空格数
-				while (lastTokenPtr->level < countSpace && lastTokenPtr->type == TokenType::LIST) {
-					tokensPtr = &(lastTokenPtr->children);
-					lastTokenPtr = &(lastTokenPtr->children.back());
-				}
-
-				if (lastTokenPtr->type != TokenType::LIST) {
-					Token tokenTemp(TokenType::LIST, isOrder ? "1" : "0", countSpace);
-
-					tokenTemp.children.push_back(tokenListCell);
-					tokensPtr->push_back(tokenTemp);
-				}
-				else {
-					//	判断 LIST 和 LISTCELL 的有无序状态是否一致
-					if ((lastTokenPtr->content == "1") == isOrder) {
-						tokensPtr->back().children.push_back(tokenListCell);
-					}
-					else {
-						Token tokenTemp(TokenType::LIST, isOrder?"1":"0", countSpace);
-						
-						tokenTemp.children.push_back(tokenListCell);
-						tokensPtr->push_back(tokenTemp);
-					}
-				}
-			}
-			else {
-				Token tokenTemp(TokenType::LIST, isOrder ? "1" : "0", countSpace);
-
-				tokenTemp.children.push_back(tokenListCell);
-				tokens.push_back(tokenTemp);
-			}
-
+			markList(tokens, lineText, pos, countSpace, isOrder);
 			parserState = parserStates::parserStateOthers;
 			break;
 		case parserStates::parserStateTable:
