453421cd021cd390a93227b7769abb9ce8176d3d
du-xiang <2876069311@qq.com>
2023-04-15 22:31:26 +0800

    添加表格标记检测

--- getTokens.h
+++ getTokens.h
@@ -5,16 +5,14 @@
 
 //  将 markdown 文本分解为Token
 std::vector<Token> markdownParser::getTokens(const std::string& mdName) {
-	std::vector<Token> tokens{};                          //  存放解析出的 Token
+	std::vector<Token> tokens{};                        //  存放解析出的 Token
 	std::string lineText;                               //  按行读取文件文本
-	bool isList = false;								//	是否处于列表解析状态
 	bool isOrder = false;								//	列表是否为有序列表
 	int pos = 0;										//	字符定位,用于列表判定
 	int countSpace = 0;									//	列表前的空格字符数
 	Token markWrap(TokenType::WRAP);					//	换行符Token
 	Token markMath(TokenType::MATH);					//	存放数学公式内容Token
 	Token markCode(TokenType::CODE);					//	存放代码内容Token
-	Token markTable(TokenType::TABLE);					//	存放表格Token
 
 	std::ifstream mdFile(mdName);                       //	打开文件
 
@@ -23,7 +21,6 @@ std::vector<Token> markdownParser::getTokens(const std::string& mdName) {
 		pos = 0;
 		countSpace = 0;
 
-
 		//	跳过空格字符并统计空格数量
 		while (lineText[pos] == ' ') {
 			pos += 1;
@@ -31,42 +28,24 @@ std::vector<Token> markdownParser::getTokens(const std::string& mdName) {
 		}
 
 		//	判定是否为列表解析器状态
-		isList = false;
-		//	有序列表
-		if (lineText[pos] >= '1' && lineText[pos] <= '9' && lineText.substr(pos + 1, 2) == ". ") {
-			isList = true;
-			isOrder = true;
-		}
-		//	无序列表
-		if (lineText.substr(pos, 2) == "* " || lineText.substr(pos, 2) == "- " || lineText.substr(pos, 2) == "+ ") {
-			isList = true;
-			isOrder = false;
-		}
-		
-		if (isList)
+		if (isList(lineText, pos, isOrder))
 			parserState = parserStates::parserStateList;
 
 		//	判断是否为换行符
 		//	需要注意的是换行符状态的检测需要在列表状态检测后
 		//	因为有些换行符的前面部分字符与列表标记一样
-		char ch		  = lineText[pos];					//	此行出现的除空格外第一个字符
-		int countChar = 0;								//	统计出现的非空格字符数
-		int posWrap	  = pos;							//	字符定位
-		if (ch == '*' || ch == '-' || ch == ' ') {
-			for (posWrap; posWrap < lineText.length(); posWrap++) {
-				if (lineText[posWrap] == ch)
-					countChar += 1;
-				else if (lineText[posWrap] != ch && lineText[posWrap] != ' ')
-					break;
-			}
-		}
-
-		if (posWrap == lineText.length() && countChar >= 3 && lineText.length() != 0)
+		if (isWrap(lineText, pos))
 			parserState = parserStates::parserStateWrap;
 
+		//	判断是否为表格
+		//	表格特征如下：
+		//	1. '|'字符数大于等于二；
+		//	2.排除空格，最前和最后一个字符必须为'|' ；
+		//	3. 表格第二行标记符只包括'|'、'-'、':'和空格
+		if (isTable(lineText))
+			parserState = parserStates::parserStateTable;
 
-
-		//	判定是否为多行数学公式或代码块
+		//	判定是否为多行数学公式
 		if (lineText == "$$") {
 			//	数学公式
 			if (parserState == parserStates::parserStateMath) {
@@ -81,7 +60,9 @@ std::vector<Token> markdownParser::getTokens(const std::string& mdName) {
 			}
 
 		}
-		else if (lineText.substr(0, 3) == "```") {
+
+		//	判定是否为代码块
+		if (lineText.substr(0, 3) == "```") {
 			//	代码块
 			if (parserState == parserStates::parserStateCode) {
 				tokens.push_back(markCode);
@@ -94,6 +75,8 @@ std::vector<Token> markdownParser::getTokens(const std::string& mdName) {
 				continue;
 			}
 		}
+
+
 
 		switch(parserState){
 		case parserStates::parserStateOthers:
@@ -120,7 +103,8 @@ std::vector<Token> markdownParser::getTokens(const std::string& mdName) {
 			parserState = parserStates::parserStateOthers;
 			break;
 		case parserStates::parserStateTable:
-			
+			markTable(tokens, lineText);
+			parserState = parserStates::parserStateOthers;
 			break;
 		case parserStates::parserStateWrap:
 			tokens.push_back(markWrap);
